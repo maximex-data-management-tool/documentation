47986a54b6ef832615559618adec976b93e577ef
Pawel Nowicki <pawel.nowicki@rodias.de>
2026-09-07 14:05:07 +0200

    6.2.2.1

---
+++ file/EAM_CHECK_IMPORT_RULE.py
@@ -0,0 +1,63 @@
+result = ""
+if (table == "LOCATIONS" and field2update == "LOCATION" and fixedKeyAttribute == "SITEID"):
+    result = "SITEID"
+if (table == "TICKETS" and field2update == "LOCATION" and fixedKeyAttribute == "SITEID"):
+    result = "ASSETSITEID"
+if (table == 'WORKORDER' and field2update == "ASSETNUM" and fixedKeyAttribute == "SITEID"):
+    result = "SITEID"
+if (table == "ASSETTRANS" and field2update == "ASSETNUM" and fixedKeyAttribute == "SITEID"):
+    result = "SITEID"
+if (table == "MULTIASSETLOCCI" and field2update == "NEWREPLACEASSETNUM" and fixedKeyAttribute == "SITEID"):
+    result = "SITEID"
+if (table == "ASSET" and field2update == "ANCESTOR" and fixedKeyAttribute == "SITEID"):
+    result = "SITEID"
+if (table == "A_ASSET" and field2update == "PARENT" and fixedKeyAttribute == "SITEID"):
+    result = "SITEID"
+if (table == "A_ASSET" and field2update == "ANCESTOR" and fixedKeyAttribute == "SITEID"):
+    result = "SITEID"
+if (table == "MULTIASSETLOCCI" and field2update == "REPLACEASSETNUM" and fixedKeyAttribute == "SITEID"):
+    result = "SITEID"
+if (table == "WORKORDER" and field2update == "ASSETNUM" and fixedKeyAttribute == "SITEID"):
+    result = "SITEID"
+if (table == "ASSETTRANS" and field2update == "ASSETNUM" and fixedKeyAttribute == "SITEID"):
+    result = "SITEID"
+if (table == "MULTIASSETLOCCI" and field2update == "ASSETNUM" and fixedKeyAttribute == "SITEID"):
+    result = "SITEID"
+if (table == "ASSETTRANS" and field2update == "FROMPARENT" and fixedKeyAttribute == "SITEID"):
+    result = "SITEID"
+if (table == "MULTIASSETLOCCI" and field2update == "ASSETNUM" and fixedKeyAttribute == "SITEID"):
+    result = "SITEID"
+if (table == "ASSETTRANS" and field2update == "FROMPARENT" and fixedKeyAttribute == "SITEID"):
+    result = "SITEID"
+if (table == "ASSETTRANS" and field2update == "TOPARENT" and fixedKeyAttribute == "SITEID"):
+    result = "TOSITEID"
+if (table == "ASSET" and field2update == "PARENT" and fixedKeyAttribute == "SITEID"):
+    result = "SITEID"
+if (table == "ASSET" and field2update == "ASSETNUM" and fixedKeyAttribute == "SITEID"):
+    result = "SITEID"
+if (table == "A_ASSET" and field2update == "ASSETNUM" and fixedKeyAttribute == "SITEID"):
+    result = "SITEID"
+if (table == "LOCATIONS" and field2update == "LOCATION" and fixedKeyAttribute == "SITEID"):
+    result = "SITEID"
+if (table == "TICKET" and field2update == "LOCATION" and fixedKeyAttribute == "SITEID"):
+    result = "ASSETSITEID"
+if (table == "MULTIASSETLOCCI" and field2update == "NEWASSETNUM" and fixedKeyAttribute == "SITEID"):
+    result = "SITEID"
+if (table == "MULTIASSETLOCCI" and field2update == "MOVETOPARENT" and fixedKeyAttribute == "SITEID"):
+    result = "SITEID"
+if (table == "LOCATIONS" and field2update == "STOREROOMPARENT" and fixedKeyAttribute == "SITEID"):
+    result = "SITEID"
+if (table == "LOCATIONS" and field2update == "REORDFROMSTORE" and fixedKeyAttribute == "SITEID"):
+    result = "SITEID"
+if (table == "INVRESERVE" and field2update == "OPLOCATION" and fixedKeyAttribute == "SITEID"):
+    result = "SITEID"
+if (table == "LABOR" and field2update == "WORKLOCATION" and fixedKeyAttribute == "SITEID"):
+    result = "WORKSITE"
+if (table == "MATRECTRANS" and field2update == "LOCATION" and fixedKeyAttribute == "SITEID"):
+    result = "SITEID"
+if (table == "MULTIASSETLOCCI" and field2update == "LOCATION" and fixedKeyAttribute == "SITEID"):
+    result = "SITEID"
+if (table == "INVUSELINE" and field2update == "LOCATION" and fixedKeyAttribute == "SITEID"):
+    result = "SITEID"
+if (table == "WORKORDER" and field2update == "LOCATION" and fixedKeyAttribute == "SITEID"):
+    result = "SITEID"
